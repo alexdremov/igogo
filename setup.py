32349530bcba2b16b50b80b9b77f382aa2aade99
alexdremov <dremov.me@gmail.com>
2023-03-22 10:09:30 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "igogo",
+    version = "0.0.1",
+    author = "Alex Dremov",
+    author_email = "igogo@alexdremov.me",
+    description = ("Execute sevaral jupyter cells simultaneously"),
+    license = "MIT",
+    keywords = "jupyterlab ipython jupyter execute tools python",
+    url = "http://packages.python.org/igogo",
+    packages=['src']
+)
